233cd011af4faed8a215944fb026c3b2b777b6ec
Pedro Almeida <pedro.almeida@viannasempre.com.br>
2024-08-28 00:03:48 -0300

    primeira tentativa exercicio

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-var teste = "pedro";
+var fraseInicio = "elefante incomoda muita gente";
+var palavra = "elefantes";
+var finalFrase = "muito mais";
+
+function imprime() {
+
+    for(var i = 1; i<100; i++)
+    {
+        console.log(fraseInicio);
+        var somaIncomoda= "incomodam";
+
+        for(var j = 1; j <= 10; j++)
+        {
+            console.log(`${j} ${palavra} ${somaIncomoda} ${finalFrase}`);
+            somaIncomoda = somaIncomoda + somaIncomoda;
+        }
+        console.log("Descrescendo");
+        
+        for(var k = 10; k > 0; k--)
+        {
+            console.log(`${k} ${palavra} ${somaIncomoda} ${finalFrase}`);
+            somaIncomoda = somaIncomoda + somaIncomoda;
+        }
+    }
+}
+
+imprime();
